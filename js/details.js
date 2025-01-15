72e46895c4c4077c6e97e0774c2a2c8ef4aebf92
Victor <zerasul@gmail.com>
2025-01-15 12:23:26 +0100

    initial commit

---
+++ js/details.js
@@ -0,0 +1,59 @@
+// Function to get query parameter by name
+function getQueryParam(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+}
+
+async function fetchData(index) {
+    try {
+        const response = await fetch('data.json');
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        return data[index];
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+function updateMetaDescription(description) {
+    
+}
+
+function renderDetails() {
+    const id = getQueryParam('id');
+
+    fetchData(id).then(data => {
+        const details = document.querySelector('#title');
+        details.innerHTML = `
+            ${data.title}
+        `;
+        const author= document.querySelector('#author');
+        author.innerHTML = `
+            Autor: ${data.author}
+        `;
+        const date = document.querySelector('#date');
+        date.innerHTML = `
+            <i>Horario: ${data.date}</i>
+        `;
+        const description = document.querySelector('#desc');
+        description.innerHTML = `
+            ${data.desc}
+        `;
+        const bio = document.querySelector('#bio');
+        bio.innerHTML = `
+            ${data.bio}
+        `;
+        if(data.img){
+            const imgponente = document.querySelector('#imgponente');
+            imgponente.src= `${data.img}`;
+        }
+        document.title= data.title;
+
+        let metaDescription = document.querySelector('meta[name="description"]');
+        if (metaDescription) {
+            metaDescription.setAttribute('content', data.desc);
+        }
+    });
+}
