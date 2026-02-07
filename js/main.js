2cc98cbd72b838c52635c37ab8eb8aaa5c48764f
Victor <zerasul@gmail.com>
2026-02-07 20:20:30 +0100

    añadiendo presentaciones charlas

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ function renderTalkDetails(data) {
     document.getElementById('hour').textContent = data.hour;
     document.getElementsByTagName('title')[0].textContent = data.name + ' - SLCENT XXII';
     for (let author of data.authors) {
-        if(!author.picture)
-            author.picture='/img/defaultuser.png'
-       const authorData=`
+        if (!author.picture)
+            author.picture = '/img/defaultuser.png'
+        const authorData = `
                 <!-- Autor 1 -->
                 <div class="author-card bg-white p-8 rounded-lg shadow-md text-center">
                     <img src="${author.picture}" alt="Foto del ponente" class="w-40 h-40 rounded-full mx-auto mb-4 object-cover bg-gray-200">
@@ -22,7 +22,22 @@ function renderTalkDetails(data) {
                         ${author.bio}
                     </p>
                 </div>`;
-         document.querySelector('#authorsGrid').insertAdjacentHTML('beforeend', authorData);
+        document.querySelector('#authorsGrid').insertAdjacentHTML('beforeend', authorData);
+
+    }
+
+    if (!data.resources || data.resources.length === 0) {
+        document.querySelector('#resourcesSection').style.display = 'none';
+    } else {
+        for (let resource of data.resources) {
+            const resourceData = `
+            <div class="resource-card bg-gray-100 p-4 rounded-lg mb-4">
+                <a href="${resource.url}" target="_blank" class="text-blue-600 font-semibold underline">
+                    - ${resource.name}
+                </a>
+            </div>`;
+            document.querySelector('#resourcesGrid').insertAdjacentHTML('beforeend', resourceData);
+        }
     }
 }
 
